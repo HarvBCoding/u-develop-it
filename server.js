bdae82cde5036f5e29dfc0f253a4598f988c5c1c
Breania Harvey <harvey.breania@gmail.com>
2021-10-22 01:33:53 -0500

    sql database connected to express

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const mysql = require('mysql2');
 
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -7,6 +8,24 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// conneact to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        // sql username,
+        user: 'root',
+        // sql password
+        password: 'MischiefManaged',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
+// the db object is using the query() method which runs the SQL query & executes the callback w/ all the resulting rows that match
+// the callback captures the responses from the query in 2 variables the err [error response] & rows [the database query response]
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
+});
 
 // Default response for any other request (Not Found)
 app.use((req, res) => {
